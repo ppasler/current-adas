da9cb810ae6834bd326f237b7d137ffe8e12b248
ppasler <paul@ppasler.de>
2016-02-23 12:32:50 +0100

    * fixed render.py color error and added gyro support * added clear button to index.html * added epoc sensor graphic

--- project/code/render.py
+++ project/code/render.py
@@ -6,6 +6,8 @@ try:
 except ImportError:
     print 'No psyco. Expect poor performance. Not really...'
 
+import sys
+import logging
 import pygame
 import platform
 from pygame import FULLSCREEN
@@ -25,14 +27,20 @@ quality_color = {
     "7": (0, 255, 0),
 }
 
-old_quality_color = {
-    "0": (0, 0, 0),
-    "1": (255, 0, 0),
-    "2": (255, 255, 0),
-    "3": (0, 255, 0),
-    "4": (0, 255, 0),
-}
+old_quality_color = {"0": (0, 0, 0)}
 
+def createQuality():
+    for i in range(4):
+        old_quality_color[str(1+i)] = (255, 0, 0)
+        old_quality_color[str(4+i)] = (255, 255, 0)
+        old_quality_color[str(8+i)] = (0, 255, 0)
+        old_quality_color[str(12+i)] = (0, 255, 0)
+
+createQuality()
+print old_quality_color
+
+resolution = (1600, 900)
+emotiv = None
 
 class Grapher(object):
     """
@@ -59,7 +67,7 @@ class Grapher(object):
         """
         Appends value and quality values to drawing buffer.
         """
-        if len(self.buffer) == 800 - self.x_offset:
+        if len(self.buffer) == resolution[0] - self.x_offset:
             self.buffer = self.buffer[1:]
         self.buffer.append([packet.sensors[self.name]['value'], packet.sensors[self.name]['quality'], packet.old_model])
 
@@ -97,16 +105,18 @@ def main():
     """
     global gheight
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode(resolution)
     graphers = []
     recordings = []
     recording = False
     record_packets = []
     updated = False
-    cursor_x, cursor_y = 400, 300
+    middle_x, middle_y = resolution[0]/2, resolution[1]/2
+    cursor_x, cursor_y = middle_x, middle_y
     for name in 'AF3 F7 F3 FC5 T7 P7 O1 O2 P8 T8 FC6 F4 F8 AF4'.split(' '):
         graphers.append(Grapher(screen, name, len(graphers)))
     fullscreen = False
+    global emotiv
     emotiv = Emotiv(display_output=False)
     gevent.spawn(emotiv.setup)
     gevent.sleep(0)
@@ -114,6 +124,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 emotiv.close()
+                pygame.quit()
                 return
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
@@ -121,10 +132,10 @@ def main():
                     return
                 elif event.key == pygame.K_f:
                     if fullscreen:
-                        screen = pygame.display.set_mode((800, 600))
+                        screen = pygame.display.set_mode(resolution)
                         fullscreen = False
                     else:
-                        screen = pygame.display.set_mode((800, 600), FULLSCREEN, 16)
+                        screen = pygame.display.set_mode(resolution, FULLSCREEN, 16)
                         fullscreen = True
                 elif event.key == pygame.K_r:
                     if not recording:
@@ -136,33 +147,36 @@ def main():
                         record_packets = None
         packets_in_queue = 0
         try:
+            cursor_x, cursor_y = middle_x, middle_y
             while packets_in_queue < 8:
                 packet = emotiv.dequeue()
                 if abs(packet.gyro_x) > 1:
-                    cursor_x = max(0, min(cursor_x, 800))
-                    cursor_x -= packet.gyro_x
+                    cursor_x += packet.gyro_x
                 if abs(packet.gyro_y) > 1:
                     cursor_y += packet.gyro_y
-                    cursor_y = max(0, min(cursor_y, 600))
                 map(lambda x: x.update(packet), graphers)
                 if recording:
                     record_packets.append(packet)
                 updated = True
                 packets_in_queue += 1
-        except Exception, ex:
-            print ex
+        except Exception as e:
+            logging.exception("154")
+            
 
         if updated:
             screen.fill((75, 75, 75))
             map(lambda x: x.draw(), graphers)
-            pygame.draw.rect(screen, (255, 255, 255), (cursor_x - 5, cursor_y - 5, 10, 10), 0)
+            cursor_x = middle_x+cursor_x/8
+            cursor_y = middle_y+cursor_y/8
+            pygame.draw.rect(screen, (255, 0, 255), [cursor_x, cursor_y, 5, 5], 5)
             pygame.display.flip()
             updated = False
         gevent.sleep(0)
 
 try:
-    gheight = 580 / 14
+    gheight = (resolution[1]-resolution[1]*0.1) / 14
     main()
-
-except Exception, e:
-    print e
+except Exception as e:
+    logging.exception("169")
+finally:
+    sys.exit(0) 
