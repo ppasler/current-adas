4d97212e4876801c82ec2fe892b90ebc6028e0ab
ppasler <paul@ppasler.de>
2016-04-12 12:40:33 +0200

    added doc for signal util

--- project/code/src/util/signal_util.py
+++ project/code/src/util/signal_util.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python
 
-import numpy as np
-
 class SignalUtil(object):
 
     def __init__(self):
         """This class does signal processing with raw signals"""
 
     def normalize(self, data):
-        '''normalizes data between -1 and 1'''
+        '''normalizes data between -1 and 1
+        @param data: array like list of values
+        
+        @return: normalized data
+        '''
         
         extreme = float(max(max(data), abs(min(data))))
         return data / extreme
 
     def energy(self, data):
-        '''calc energy E = sum(data ** 2) https://en.wikipedia.org/wiki/Energy_(signal_processing)'''
+        '''calc energy E = sum(data ** 2) https://en.wikipedia.org/wiki/Energy_(signal_processing)
+        
+        @param data: array like list of values
+        
+        @return: signal energy
+        
+        '''
         
         return sum(data ** 2)
